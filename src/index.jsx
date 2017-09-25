e669f90cd086f457892fff4899ed20e15606ad21
SkoebaSteve <janssenman@gmail.com>
2017-09-25 20:23:11 +0200

    basic setup with react, babel transpiling and webpack

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+/* eslint no-unused-vars: 1 */
+import React from 'react'
+import {render} from 'react-dom'
+
+class ContactSearch extends React.Component {
+  render () {
+    return <h1>Contact search</h1>
+  }
+}
+
+render(<ContactSearch />, document.getElementById('app'))
