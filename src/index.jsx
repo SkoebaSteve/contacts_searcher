2c704d55eaa8d87f08ec810e674f2a98b0923a01
SkoebaSteve <janssenman@gmail.com>
2017-09-28 11:19:43 +0200

    added webpack server, linting rules and basic filter option through textfield

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,70 @@
-/* eslint no-unused-vars: 1 */
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+
+const userArray = [
+  {firstName: "steef", lastName: "Janssen", phoneNumber: "012345678"}
+]
+
+const SearchForm = ({onChange}) => {
+  return (
+    <form>
+      <input
+        type="search"
+        onChange={event => onChange(event.target.value)}
+      />
+    </form>
+  )
+}
+
+const User = ({firstName, lastName, phoneNumber}) => {
+  return <li>{firstName} {lastName} - {phoneNumber}</li>
+}
+
+const SearchResults = ({users}) => {
+  return (
+    <ul>
+      {
+        users.map(function(user) {
+          return (
+            <User
+              key={user.phoneNumber}
+              firstName={user.firstName}
+              lastName={user.lastName}
+              phoneNumber={user.phoneNumber}
+            />
+          )
+        })
+      }
+    </ul>
+  )
+}
 
 class ContactSearch extends React.Component {
+
+  state = {
+    users: userArray
+  }
+
+  onChange = (value) => {
+    const updatedUsers = userArray.filter(function(user) {
+      if (user.firstName.indexOf(value) !== -1) {
+        return user
+      }
+      return null
+    })
+
+    this.setState({
+      users:updatedUsers
+    })
+  }
+  
   render () {
-    return <h1>Contact search</h1>
+    return (
+      <div>
+        <SearchForm onChange={this.onChange} />
+        <SearchResults users={this.state.users} />
+      </div>
+    )
   }
 }
 
